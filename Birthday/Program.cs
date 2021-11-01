be57689626f6af85c691d8542844c34ba40be6b4
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-11-01 13:48:21 +0200

    Add files via upload

---
+++ Birthday/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Birthday
+{
+    class Program
+    {
+        public static string all;
+        public static string[] s;
+        static void Main(string[] args)
+        {
+            List<string> vs = new List<string>();
+            string end = "End";
+            string who;
+            do
+            {
+                all = Console.ReadLine();
+                s = all.Split(' ');
+                vs.Add(s[s.Length - 1]);
+            }
+            while (all != end);
+            who = Console.ReadLine();
+            Console.WriteLine();
+            for (int i = 0; i < vs.Count; i++)
+            {
+                if (vs[i].EndsWith(who))
+                {
+                    Console.WriteLine(vs[i].Substring(s.Length - 1, vs[i].Length));
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
