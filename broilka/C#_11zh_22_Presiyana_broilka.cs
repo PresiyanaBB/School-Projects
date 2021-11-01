d0e33c1010f4b112f7e34574915f867ac8554f07
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-11-01 13:48:35 +0200

    Add files via upload

---
+++ broilka/C#_11zh_22_Presiyana_broilka.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace broilka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            Queue<int> deca = new Queue<int>();
+            for(int i = 1 ; i <= n ; i++)
+            {
+                deca.Enqueue(i);
+            }
+            while (deca.Count != 1)
+            {
+                for (int i = 1; i <= m; i++)
+                {
+                    if (i == m) deca.Dequeue();
+                    else
+                    {
+                        deca.Enqueue(deca.Dequeue());
+                    }
+                }
+            }
+            Console.WriteLine(deca.Dequeue());
+        }
+    }
+}
