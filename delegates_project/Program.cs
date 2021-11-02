9a066bde38a6f47635cf2e03bf53669a840d61d1
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-11-02 11:56:48 +0200

    Add files via upload

---
+++ delegates_project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace delegates_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "1wi opit";
+            Action<string> del = Method1;
+            del(a);
+
+        }
+        public static void Method1(string pechat)
+        {
+            Console.WriteLine(pechat);
+        }
+    }
+}
