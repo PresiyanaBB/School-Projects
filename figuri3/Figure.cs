3961f02c1bd1e305741253cb3a05ede79b11e487
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-11-07 14:28:14 +0200

    Add files via upload

---
+++ figuri3/Figure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace figuri2
+{
+    abstract class Figure
+    {
+        protected double side;
+        protected double side2;
+        protected double side3;
+        public virtual void Input()
+        {
+            Console.Write("a: ");
+            side = double.Parse(Console.ReadLine());
+        }
+        public abstract double Lice();
+        public abstract double Perimetyr();
+    }
+}
---
+++ figuri3/Kvadrat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace figuri2
+{
+    class Kvadrat : Figure
+    {
+        public override void Input()
+        {
+            base.Input();
+        }
+        public override double Lice()
+        {
+            Console.WriteLine("Лицето на квадрата е: ");
+            return (double)side * side;
+        }
+        public override double Perimetyr()
+        {
+            Console.WriteLine("Периметъра на квадрата е: ");
+            return (double)4 * side;
+        }
+    }
+}
