9a9dc0db87e7aa993f30ddf09b658434bb5470dc
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-11-07 14:28:51 +0200

    Add files via upload

---
+++ horse_moves/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace horse_moves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var board = new bool [n,n];
+            Horse(board,0,n);
+        }
+
+        private static void Horse(bool[,] board,int row,int col)
+        {
+            if (AllVisited() == true)
+            {
+                Console.WriteLine(AllPaths(board,row,col));
+                return;
+            }
+        }
+
+        private static bool AllPaths(bool[,] board, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool AllVisited()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
