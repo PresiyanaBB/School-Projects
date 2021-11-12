d0b19ebd6a884407361ee699b54d357ab6225d92
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-11-12 16:38:09 +0200

    Add files via upload

---
+++ lottery/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int[] a = new int[num];
+            int[] b = new int[num];
+            string input_a = Console.ReadLine().Split().ToString();
+            string input_b = Console.ReadLine().Split().ToString();
+            Stack<int> c = new Stack<int>();
+            do
+            {
+
+            }
+            while (c.Count != num);
+        }
+    }
+}
